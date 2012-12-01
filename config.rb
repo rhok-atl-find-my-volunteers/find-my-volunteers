164fcb67fec066d32fe0c3e40575cd697d605689
Chris Breiding <chrisbreiding@gmail.com>
2012-12-01 13:55:53 -0500

    add base css

---
+++ config.rb
@@ -0,0 +1,8 @@
+relative_assets       = true
+
+http_images_path      = '/images'
+http_stylesheets_path = '/css'
+
+css_dir               = File.join 'public', 'css'
+sass_dir              = File.join 'public', 'css'
+images_dir            = File.join 'public', 'images'
