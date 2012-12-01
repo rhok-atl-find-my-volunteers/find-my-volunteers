77cdba2feae9b6f423beeabf5d2c5d9b4cde145b
Tej <vishnutej+git@gmail.com>
2012-12-01 14:50:09 -0500

    serve up static serve index.html from /

--- server.coffee
+++ server.coffee
@@ -2,8 +2,12 @@ express = require 'express'
 app = express()
 app.enable 'trust proxy'
 
+app.configure(->
+  app.use(app.router)
+  app.use(express.static(__dirname + '/public'))
+)
 app.get '/', (req, res)->
-  res.send 'Hello World!'
+  res.sendfile(__dirname + '/public/index.html')
 
 app.listen 3000 or process.env.PORT
 console.log 'listening...'
--- server.js
+++ server.js
@@ -7,8 +7,13 @@
 
   app.enable('trust proxy');
 
+  app.configure(function() {
+    app.use(app.router);
+    return app.use(express["static"](__dirname + '/public'));
+  });
+
   app.get('/', function(req, res) {
-    return res.send('Hello World!');
+    return res.sendfile(__dirname + '/public/index.html');
   });
 
   app.listen(3000 || process.env.PORT);
